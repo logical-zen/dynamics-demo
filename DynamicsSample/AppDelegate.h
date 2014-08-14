6e7d654947b4cb41f5977210f69d245c8e8b366b
Andria Jensen <andria@logicalzen.com>
2014-08-14 15:16:02 -0400

    Initial Commit

---
+++ DynamicsSample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DynamicsSample
+//
+//  Created by Andria Jensen on 8/14/14.
+//  Copyright (c) 2014 Logical Zen, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DynamicsSample/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DynamicsSample
+//
+//  Created by Andria Jensen on 8/14/14.
+//  Copyright (c) 2014 Logical Zen, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
